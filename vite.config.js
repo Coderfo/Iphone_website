fbdc9d1dcbad4d0ec151e707c0782daaaea2b62d
Coderfo <95329424+Coderfo@users.noreply.github.com>
2025-03-08 16:49:42 -0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "miguel-2c",
-    base: "/Iphone_website/",
-    project: "javascript-react"
+    base: "/Iphone_website/"
   })],
 
   build: {
