f8f6448bef42427bdf81daa3d7724c5750979120
Coderfo <95329424+Coderfo@users.noreply.github.com>
2025-03-08 16:10:09 -0600

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import React from 'react'
 import './index.css'
 import App from './App.jsx'
-
 import * as Sentry from "@sentry/react";
 
 Sentry.init({
@@ -28,8 +27,6 @@ Sentry.init({
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    
     <App/>
-  
   </StrictMode>,
 )
